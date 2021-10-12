f32475fd246f316f5e725566d6d6c8244f140065
Dueey <2103duykim@gmail.com>
2021-10-12 11:32:36 -0700

    Finished basic template for contacts page

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,33 +1,24 @@
 import React, { useState } from "react";
 import styled from "styled-components";
+import { ArrowBack } from "@material-ui/icons";
 
 function Contact() {
   const [hidden, setHidden] = useState("active");
   const [hidden2, setHidden2] = useState("hidden");
+  const [arrow, setArrow] = useState("hideArrow");
 
   function handleClick() {
     if (hidden === "hidden") {
       setHidden("active");
       setHidden2("hidden");
+      setArrow("hideArrow");
       // setHidden3("hidden");
       // setHidden4("hidden");
       // setHidden5("hidden");
     } else if (hidden === "active") {
       setHidden("hidden");
       setHidden2("active");
-    }
-  }
-
-  function handleClick2() {
-    if (hidden === "hidden") {
-      setHidden("active");
-      setHidden2("hidden");
-      // setHidden3("hidden");
-      // setHidden4("hidden");
-      // setHidden5("hidden");
-    } else if (hidden === "active") {
-      setHidden("hidden");
-      setHidden2("active");
+      setArrow("showArrow");
     }
   }
 
@@ -38,7 +29,15 @@ function Contact() {
           <div className={hidden} onClick={handleClick}>
             <h1>Contact Me</h1>
           </div>
-          <div className={hidden2}></div>
+          <div className={hidden2}>
+            <div className='info'>
+              <ArrowBack className={arrow} onClick={handleClick} />
+              <span>Email (Preferred Contact): 2103duykim@gmail.com</span>
+              <span>Phone: (714) 944-0018</span>
+              <span>LinkedIn: 2103duykim@gmail.com</span>
+              <span>Indeed: 2103duykim@gmail.com</span>
+            </div>
+          </div>
         </Details>
       </Wrapper>
     </Container>
@@ -80,11 +79,42 @@ const Details = styled.div`
       font-size: 150px;
       font-family: "Lobster", cursive;
       color: gray;
+      cursor: pointer;
+    }
+
+    .info {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-self: center;
+
+      span {
+        color: white;
+        font-weight: bold;
+        font-size: 20px;
+      }
+
+      .showArrow {
+        color: white;
+        cursor: pointer;
+        font-size: 30px;
+        position: absolute;
+        margin-left: -155px;
+        margin-top: -105px;
+
+        :hover {
+          color: #f0b851;
+        }
+      }
+
+      .hideArrow {
+        display: none;
+      }
     }
   }
 
   .active {
-    border: 1px solid #ed6a61;
+    border: 1px solid #f0b851;
     background-color: #29292c;
     height: 250px;
     width: 800px;
